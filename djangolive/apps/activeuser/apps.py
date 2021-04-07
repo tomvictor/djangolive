2b7a640b7b655a607057fabef3a9d5c48e52cdea
Tom Victor <vjtomvictor@icloud.com>
2021-04-07 23:10:26 +0530

    Code Refactoring and new apps

--- djangolive/apps/activeuser/apps.py
+++ djangolive/apps/activeuser/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DjangoliveConfig(AppConfig):
+class ActiveuserConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "djangolive"
+    name = "apps.activeuser"
