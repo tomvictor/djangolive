b10de482fac55ae087c9e48c7c7491ddbc4b9172
Tom Victor <vjtomvictor@icloud.com>
2021-04-16 21:58:14 +0530

    Added  reference test cases

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,6 +29,7 @@ def pytest_configure():
         MIDDLEWARE=MIDDLEWARE,
         MIDDLEWARE_CLASSES=MIDDLEWARE,
         INSTALLED_APPS=(
+            "django.contrib.admin",
             "django.contrib.auth",
             "django.contrib.contenttypes",
             "django.contrib.sessions",
