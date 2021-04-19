8b1874e160afe955a1e807bbe0a1408786666c93
Tom Victor <vjtomvictor@icloud.com>
2021-04-19 08:19:22 +0530

    Create profiler.py

---
+++ djangolive/utils/profiler.py
@@ -0,0 +1,19 @@
+import time
+import functools
+
+
+def time_profile(func):
+    """Time Profiled for optimisation
+
+   Notes:
+        * Do not use this in production
+
+    """
+    @functools.wraps(func)
+    def profile(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} : {time.time() - start}")
+        return result
+
+    return profile
